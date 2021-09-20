eb7f86775f1aeb28ba58586d70deb7159aa90a7c
jack01245 <2080852186@qq.com>
2021-09-20 10:50:16 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+//懒加载
+const Home = () => import('@/views/home/Home')
+const Category = () => import('@/views/category/Category')
+const ShopCart = () => import('@/views/shopCart/ShopCart')
+const Detail = () => import('@/views/detail/Detail')
+const Profile = () => import('@/views/profile/Profile')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/home',
+    meta: {
+      showTab: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home,
+    meta: {
+      showTab: true
+    }
+  },
+  {
+    path: '/category',
+    component: Category,
+    meta: {
+      showTab: true
+    }
+  },
+  {
+    path: '/shopcart',
+    component: ShopCart,
+    meta: {
+      showTab: true
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      showTab: true
+    }
+  },
+  {
+    path: '/detail',
+    component: Detail,
   }
 ]
 
---
+++ src/views/shopCart/ShopCart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="shop-cart">
+    购物车
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShopCart"
+}
+</script>
+
+<style scoped>
+
+</style>
